70ff448b17fef1f17c3ab5df51c51b3076f07d7d
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-10-25 10:06:20 -0700

    add and define variables in js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+ $(".form-control").submit(function(event) {
+
+var color = parseInt($(input#color).val());
+var usage = parseInt($(input#usage.val());
+var amigo = parseInt($(input#amigo).val());
+var hogwarts = parseInt($(input#hogwarts).val());
+var instructor = parseInt($(input#instructor).val());
+
+   event.preventDefault();
+
+ });
+ });

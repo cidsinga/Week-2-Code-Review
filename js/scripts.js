cae31058ef26e3fcbd8a08b143f8bc1a55f97404
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-10-25 10:44:52 -0700

    add begging of else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,25 @@ $(document).ready(function() {
 
 
  $(".form-control").submit(function(event) {
+      event.preventDefault();
 
-var color = parseInt($(input#color).val());
-var usage = parseInt($(input#usage.val());
-var amigo = parseInt($(input#amigo).val());
-var hogwarts = parseInt($(input#hogwarts).val());
-var instructor = parseInt($(input#instructor).val());
+var color = parseInt($("#color option:selected").val());
+var usage = parseInt($("#usage option:selected").val());
+var amigo = parseInt($("#amigo option:selected").val());
+var hogwarts = parseInt($("#hogwarts option:selected").val());
+var instructor = parseInt($("#instructor option:selected").val());
 
-   event.preventDefault();
+if (color || usage || amigo || hogwarts || instructor === 0) {
+    alert("Please make a selection from each box")
+}
+
+console.log()
+var result = (color + usage + amigo + hogwarts + instructor);
+
+
+
+
+
 
  });
  });

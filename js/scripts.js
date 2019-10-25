4fec0cc234d7466f8fc8244a0c6b1edd61806f0f
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-10-25 11:14:05 -0700

    refinement of if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,23 @@ var usage = parseInt($("#usage option:selected").val());
 var amigo = parseInt($("#amigo option:selected").val());
 var hogwarts = parseInt($("#hogwarts option:selected").val());
 var instructor = parseInt($("#instructor option:selected").val());
+var result = (color + usage + amigo + hogwarts + instructor);
+console.log(result)
+// if (color || usage || amigo || hogwarts || instructor === 0) {
+    // alert("Please make a selection from each box");
 
-if (color || usage || amigo || hogwarts || instructor === 0) {
-    alert("Please make a selection from each box")
+if (result <5) {
+  alert("Please make a selection from each box")
+}
+} else if (result < 9) {
+  $(".JavaScript").show();
+} else if (result <13) {
+  $(".python").show();
+} else if (result < 16) {
+  $(".ruby").show();
 }
 
 console.log()
-var result = (color + usage + amigo + hogwarts + instructor);
-
-
-
-
-
 
  });
  });

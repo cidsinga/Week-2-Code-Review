f98094049f91c8ef0ca67d5a0001b587be43cb47
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-10-25 11:43:57 -0700

    add some css styling

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,21 @@ var hogwarts = parseInt($("#hogwarts option:selected").val());
 var instructor = parseInt($("#instructor option:selected").val());
 var result = (color + usage + amigo + hogwarts + instructor);
 console.log(result)
-// if (color || usage || amigo || hogwarts || instructor === 0) {
-    // alert("Please make a selection from each box");
 
-if (result <5) {
-  alert("Please make a selection from each box")
-}
+if (color, usage, amigo, hogwarts, instructor === 0) {
+    alert("Please make a selection from each box");
+
+// if (result <5) {
+//   alert("Please make a selection from each box");
 } else if (result < 9) {
   $(".JavaScript").show();
+  $(".Python .Ruby").hide();
 } else if (result <13) {
-  $(".python").show();
+  $(".Python").show();
+  $(".JavaScript .ruby").hide();
 } else if (result < 16) {
-  $(".ruby").show();
+  $(".Ruby").show();
+  $(".JavaScript .Python").hide()
 }
 
 console.log()
